006ebac47140907bbb312bc32cd59a8295ba6567
Koo bonik <kbi960130@naver.com>
2017-07-01 11:50:49 +0000

    자바 되새기기

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello goorm!");
+		int a=3, b=4;
+		System.out.println(a+b);
+		math hll= new math();
+		hll.math();
 	}
 
 }
