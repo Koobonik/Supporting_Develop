45ebb63fd0ea8974eeee3d13175ee7d7eb5ebb41
Koo bonik <kbi960130@naver.com>
2017-07-06 21:54:53 +0000

    주사위 게임 만들기 추가

--- src/project/Main.java
+++ src/project/Main.java
@@ -10,8 +10,13 @@ public class Main {
 		System.out.println("Hello goorm!");
 		int a=3, b=4;
 		System.out.println(a+b);
-		math hll= new math();
+		
+		math hll= new math();//math 클래스 활용
 		hll.math();
+		
+		//주사위 게임 만들기
+		
 	}
+
 
 }
