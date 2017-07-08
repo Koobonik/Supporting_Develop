a0df80ec34db5f223e0a7d9d336e53ead7f92045
Koo bonik <kbi960130@naver.com>
2017-07-08 21:36:54 +0000

    Main 소스코드 간결화

--- src/project/Main.java
+++ src/project/Main.java
@@ -9,19 +9,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello goorm!");
-		int a=3, b=4;
-		System.out.println(a+b);
 		
-		math hll= new math();//math 클래스 활용
-		hll.math();
 		
 		//주사위 게임 만들기
-		DiceGame DG = new DiceGame();//주사위 객체 생성
-		int userValue=DG.makeDiceNumber();//주사위 숫자 생성
-		int comValue=DG.makeDiceNumber();
-		System.out.println("유저의 주사위 숫자 : "+userValue);
-		System.out.println("컴퓨터의 주사위 숫자 : "+comValue);
-		
+		DiceGame.DiceGame_logic();
 		
 		//계산기 만들기
 		SimpleCalculator.SimpleCalculator_logic();
