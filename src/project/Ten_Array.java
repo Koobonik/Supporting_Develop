b1d7be9331178321f89f9b0be106d12c2f9a73dd
Koo bonik <kbi960130@naver.com>
2017-07-08 08:12:53 +0000

    최대값 찾는 소스 추가

--- src/project/Ten_Array.java
+++ src/project/Ten_Array.java
@@ -4,22 +4,19 @@ public class Ten_Array{
 	static void randomNumber(){
 		int[] a = new int[10];
 		int min=100;
+		int max=0;
 		for(int i=0; i<a.length; i++){
 			a[i]=(int) (Math.random()*100 )+1;//임의의 수 대입
-			min=a[i];//최저변수에 값 대입
 			System.out.print(a[i]+" ");//임의의 수 출력
 		}
-		for(int i=0; i<a.length; i++){
-			if(min==100)
-			{
+		for(int i=0; i<a.length; i++){//최저 최대값 대입
+			if(min>a[i])
 				min=a[i];
-			}
-			else if(min>a[i])
-			{
-				min=a[i];
-			}
+			if(max<a[i])
+				max=a[i];
 		}
 		System.out.println("최저값은 "+min+" 입니다.");
+		System.out.println("최대값은 "+max+" 입니다.");
 		
 	}
 	
