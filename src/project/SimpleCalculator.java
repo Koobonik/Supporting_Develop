3384a391b93bd2d0850fabd7faa5d2c6f9d8761b
Koo bonik <kbi960130@naver.com>
2017-07-08 03:52:53 +0000

    계산기 기능 함수

---
+++ src/project/SimpleCalculator.java
@@ -0,0 +1,17 @@
+package project;
+
+public class SimpleCalculator{
+	static double plus(double d1, Double d2){
+		return d1+d2;
+	}
+	static double minus(double d1, Double d2){
+		return d1-d2;
+	}
+	static double multi(double d1, Double d2){
+		return d1*d2;
+	}
+	static double div(double d1, Double d2){
+		return d1/d2;
+	}
+	
+}
